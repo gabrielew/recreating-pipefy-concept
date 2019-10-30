fb1753b0110221517317423114f4333f8ffe8a52
Gabriel Alcântara <gabrielalcantaratp@gmail.com>
2019-10-30 09:28:03 -0300

    pipefy with drag n' drop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { DndProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+
+import GlobalStyle from "./styles/global";
+
+import Header from "./components/Header";
+import Board from "./components/Board";
+
 function App() {
 	return (
-		<div className="App">
-			<h1>oi</h1>
-		</div>
+		<DndProvider backend={HTML5Backend}>
+			<Header />
+			<Board />
+
+			<GlobalStyle />
+		</DndProvider>
 	);
 }
 
